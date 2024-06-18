6bdeb6ceb9c236ea251dae5485b7f1197172e85b
Robert <robertwinters767@gmail.com>
2024-06-18 19:05:55 -0400

    Made your code better, move buttons into place next

--- TikTakToe/src/App.tsx
+++ TikTakToe/src/App.tsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <>
       <Grid/>
-      <Buttons/>
+      <Buttons Top="50%" Left="50%"/>
     </>
   )
 }
--- TikTakToe/src/button.tsx
+++ TikTakToe/src/button.tsx
@@ -1,14 +1,28 @@
+import React, { useState } from 'react';
 import "./button.css"
 
-function Buttons() {
-  const tableValues = {
-    top: '50%',
-    left: '50%',
-  }
+interface Props {
+    Top: string;
+    Left: string;
+}
+
+function Buttons({ Top, Left }: Props) {
+  
+  const [top, setTop] = useState(Top);
+  const [left, setLeft] = useState(Left);
+
+  const handleClick = (newTop: string, newLeft: string) => {
+      setTop(newTop);
+      setLeft(newLeft);
+  };
 
   return (
     <>
-      <button className="theButton" style={{ left: tableValues.left, top: tableValues.top }}></button>
+      <button 
+      className="theButton"
+      onClick={() => handleClick('70%', '30%')}
+      style={{ left: left, top: top }}
+      ></button>
     </>
   );
 }
